7d57b9a149b914563ad25ad4e70de635d14d1f7c
VadimFarutin <vfarut@gmail.com>
2020-03-25 22:43:18 +0300

    batch_size=40

--- runGMExperiments.py
+++ runGMExperiments.py
@@ -76,7 +76,7 @@ def test_GM18526():
                         output_marks=[output_mark],
                         train_params={
                             'nb_epoch': 20,
-                            'batch_size': 10,
+                            'batch_size': 40,
                             'validation_split': 0.2,
                             'wandb_log': wandb_log
                         },
