e106c6a96be98b1b483f7ffe1eb5317dde381af8
VadimFarutin <vfarut@gmail.com>
2020-03-27 01:01:01 +0300

    Small dataset

--- runGMExperiments.py
+++ runGMExperiments.py
@@ -65,8 +65,8 @@ def test_GM18526():
                         dataset_params={
                             'train_dataset_name': 'GM12878_5+1marks-K4me3_all',
                             'test_dataset_name': '%s_5+1marks-K4me3_all' % test_cell_line,
-                            'num_train_examples': 100000,
-                            'seq_length': 1001,
+                            'num_train_examples': 1000,
+                            'seq_length': 101,
                             'peak_fraction': 0.5,
                             'train_X_subsample_target_string': subsample_target_string,
                             'num_bins_to_test': None,
@@ -84,7 +84,7 @@ def test_GM18526():
                         predict_binary_output=predict_binary_output,
                         zero_out_non_bins=True,
                         generate_bigWig=False,
-                        pretrained_model_path='./models/weights/encoder-decoder-20200325-194357397955-weights.pt')
+                        pretrained_model_path=None)
 
                     if wandb_log:
                         group = "peaks" if predict_binary_output else "signal"
