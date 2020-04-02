b81fc4f8f26caf8ecf803a84848af5d5d68d81e7
VadimFarutin <vfarut@gmail.com>
2020-04-02 14:14:01 +0300

    nb_epoch=7

--- runGMExperiments.py
+++ runGMExperiments.py
@@ -76,7 +76,7 @@ def test_GM18526():
                         },
                         output_marks=[output_mark],
                         train_params={
-                            'nb_epoch': 30,
+                            'nb_epoch': 7,
                             'batch_size': 100,
                             'validation_split': 0.2,
                             'wandb_log': wandb_log
