78594bef47c87b85ec155d077a984f8488475ae1
VadimFarutin <vfarut@gmail.com>
2020-04-05 01:59:52 +0300

    Full dataset

--- runGMExperiments.py
+++ runGMExperiments.py
@@ -65,8 +65,8 @@ def test_GM18526():
                         dataset_params={
                             'train_dataset_name': 'GM12878_5+1marks-K4me3_all',
                             'test_dataset_name': '%s_5+1marks-K4me3_all' % test_cell_line,
-                            'num_train_examples': 1000,
-                            'seq_length': 101,
+                            'num_train_examples': 100000,
+                            'seq_length': 1001,
                             'peak_fraction': 0.5,
                             'train_X_subsample_target_string': subsample_target_string,
                             'num_bins_to_test': None,
@@ -76,8 +76,8 @@ def test_GM18526():
                         },
                         output_marks=[output_mark],
                         train_params={
-                            'nb_epoch': 20,
-                            'batch_size': 100,
+                            'nb_epoch': 30,
+                            'batch_size': 110,
                             'validation_split': 0.2,
                             'wandb_log': wandb_log
                         },
