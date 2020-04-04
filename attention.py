e455727dc1d0adc0bcbfe6978aad2b177a274cd5
VadimFarutin <vfarut@gmail.com>
2020-04-05 01:54:23 +0300

    Scaled dot product in attention

--- attention.py
+++ attention.py
@@ -1,26 +1,28 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from diConstants import DEVICE
 
 
 class Attention(nn.Module):
-    def __init__(self, hidden_size, key_size, query_size):
+    def __init__(self, hidden_size, key_size, query_size, value_size):
         super(Attention, self).__init__()
 
+        self.n = torch.sqrt(torch.tensor([float(hidden_size)])).to(DEVICE)
         self.key_layer = nn.Linear(key_size, hidden_size, bias=False)
         self.query_layer = nn.Linear(query_size, hidden_size, bias=False)
-        self.energy_layer = nn.Linear(hidden_size + hidden_size, 1, bias=False)
+        self.value_layer = nn.Linear(value_size, hidden_size, bias=False)
 
-    def forward(self, query, proj_key, value, mask):
-        query = self.query_layer(query)
-
-        # scores = self.energy_layer(torch.tanh(proj_key + cur_embed?))
+    def forward(self, query, key, value, mask):
         # print("query", query.repeat(1, proj_key.shape[1], 1).shape)
         # print("proj_key", proj_key.shape)
         # print("query + proj_key", torch.cat([query.repeat(1, proj_key.shape[1], 1), proj_key], dim=2).shape)
-        scores = self.energy_layer(torch.tanh(torch.cat([query.expand(-1, proj_key.shape[1], -1), proj_key], dim=2)))
+
+        query = self.query_layer(query)
+        # scores = self.energy_layer(torch.tanh(torch.cat([query.expand(-1, proj_key.shape[1], -1), proj_key], dim=2)))
+        scores = torch.bmm(key, query.view(key.shape[0], -1, 1)) / self.n
+        
         scores = scores.squeeze(2).unsqueeze(1)
-        # scores.data.masked_fill_(mask == 0, -float('inf'))
         scores = F.softmax(scores, dim=-1)
         context = torch.bmm(scores, value)
 
--- attentionDecoder.py
+++ attentionDecoder.py
@@ -32,7 +32,8 @@ class AttentionDecoder(nn.Module):
         # self.embedding = nn.Embedding(out_channels, hidden_size)
         self.attention = Attention(hidden_size=hidden_size,
                                    key_size=hidden_size * 2 if bidirectional else hidden_size,
-                                   query_size=hidden_size)
+                                   query_size=hidden_size,
+                                   value_size=hidden_size * 2 if bidirectional else hidden_size)
 
         self.lstm = nn.LSTM(input_size=out_channels + (hidden_size * 2 if bidirectional else hidden_size),
                             hidden_size=hidden_size,
@@ -53,15 +54,15 @@ class AttentionDecoder(nn.Module):
 
         self.device = DEVICE
 
-    def forward_step(self, input, hidden, encoder_output, projection_key, projection_key_mask, encoder_output_mask):
+    def forward_step(self, input, hidden, key, value, projection_key_mask, encoder_output_mask):
         query = hidden[0][0].unsqueeze(1)
         # query = hidden[0][-1].unsqueeze(1)
         # projection_key_masked = projection_key.gather(1, projection_key_mask)
         # encoder_output_masked = encoder_output.gather(1, encoder_output_mask)
-        projection_key_masked = projection_key
-        encoder_output_masked = encoder_output
+        # projection_key_masked = projection_key
+        # encoder_output_masked = encoder_output
         # print(encoder_output_masked.shape)
-        context = self.attention(query=query, proj_key=projection_key_masked, value=encoder_output_masked, mask=None)
+        context = self.attention(query=query, key=key, value=value, mask=None)
         lstm_input = torch.cat([input.float(), context], dim=2)
         # print("lstm_input", lstm_input.shape)
         # print("hidden", hidden[0].shape)
@@ -78,9 +79,10 @@ class AttentionDecoder(nn.Module):
         # last_hidden = hidden
 
         batch_size = hidden[0].shape[1]
-        y_hat = torch.tensor([]).to(self.device)
+        y_hat = []
         y = self.SOS.view(1, 1, -1).expand(batch_size, -1, -1).float()
-        projection_key = self.attention.key_layer(encoder_output)
+        key = self.attention.key_layer(encoder_output)
+        value = self.attention.value_layer(encoder_output)
 
         teacher_forcing = np.random.uniform(0.0, 1.0) <= self.teacher_forcing_p
 
@@ -96,9 +98,10 @@ class AttentionDecoder(nn.Module):
                 #     .transpose(0, 1)\
                 #     .repeat(batch_size, 1, 1).clamp(0, seq_length - 1).long()
 
-                y_hat_i, last_hidden = self.forward_step(y, last_hidden, encoder_output, projection_key, None, None)
-                y_hat = torch.cat((y_hat, y_hat_i), dim=1)
-
+                y_hat_i, last_hidden = self.forward_step(y, last_hidden, key, value, None, None)
+                # y_hat = torch.cat((y_hat, y_hat_i), dim=1)
+                y_hat.append(y_hat_i)
+                
                 y = y_hat_i  # .detach()
         else:
             for i in range(seq_length):
@@ -112,9 +115,11 @@ class AttentionDecoder(nn.Module):
                 #     .transpose(0, 1) \
                 #     .repeat(batch_size, 1, 1).clamp(0, seq_length - 1).long()
 
-                y_hat_i, last_hidden = self.forward_step(y, last_hidden, encoder_output, projection_key, None, None)
-                y_hat = torch.cat((y_hat, y_hat_i), dim=1)
+                y_hat_i, last_hidden = self.forward_step(y, last_hidden, key, value, None, None)
+                # y_hat = torch.cat((y_hat, y_hat_i), dim=1)
+                y_hat.append(y_hat_i)
 
                 y = target.gather(1, torch.tensor([i] * batch_size).view(-1, 1, 1).to(self.device))
 
+        y_hat = torch.cat(y_hat, dim=1)
         return y_hat
