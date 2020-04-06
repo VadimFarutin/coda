34bd8e96f7ed18a1a490e36453dbba3414503664
VadimFarutin <vfarut@gmail.com>
2020-04-07 02:59:47 +0300

    'num_layers': 4 on 1e6 samples

--- runGMExperiments.py
+++ runGMExperiments.py
@@ -65,7 +65,7 @@ def test_GM18526():
                         dataset_params={
                             'train_dataset_name': 'GM12878_5+1marks-K4me3_all',
                             'test_dataset_name': '%s_5+1marks-K4me3_all' % test_cell_line,
-                            'num_train_examples': 100000,
+                            'num_train_examples': 1000000,
                             'seq_length': 101,
                             'peak_fraction': 0.5,
                             'train_X_subsample_target_string': subsample_target_string,
@@ -76,7 +76,7 @@ def test_GM18526():
                         },
                         output_marks=[output_mark],
                         train_params={
-                            'nb_epoch': 4,
+                            'nb_epoch': 10,
                             'batch_size': 256,
                             'validation_split': 0.2,
                             'wandb_log': wandb_log
@@ -84,8 +84,8 @@ def test_GM18526():
                         predict_binary_output=predict_binary_output,
                         zero_out_non_bins=True,
                         generate_bigWig=False,
-                        # pretrained_model_path=None)
-                        pretrained_model_path='./models/weights/encoder-decoder-20200406-225004364456-weights.pt')
+                        pretrained_model_path=None)
+                        # pretrained_model_path='./models/weights/encoder-decoder-20200406-225004364456-weights.pt')
 
                     if wandb_log:
                         group = "peaks" if predict_binary_output else "signal"
