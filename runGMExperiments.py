e43d215caada8482193c61d2afa6a45bb751739b
VadimFarutin <vfarut@gmail.com>
2020-04-09 15:08:40 +0300

    model=lstm

--- runGMExperiments.py
+++ runGMExperiments.py
@@ -45,7 +45,7 @@ def test_GM18526():
         for subsample_target_string in ['0.5e6']:
             for predict_binary_output in [False]:
                 for output_mark in GM_MARKS:                            
-                    model_type = 'encoder-decoder'
+                    model_type = 'lstm'
                     wandb_log = True
                     evaluate = True
                     preset_params = MODEL_PRESET_PARAMS[model_type]
@@ -76,8 +76,8 @@ def test_GM18526():
                         },
                         output_marks=[output_mark],
                         train_params={
-                            'nb_epoch': 10,
-                            'batch_size': 256,
+                            'nb_epoch': 2,
+                            'batch_size': 100,
                             'validation_split': 0.2,
                             'wandb_log': wandb_log
                         },
