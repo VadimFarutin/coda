dad4c16344ea3d970ab291e1413a177c60276455
VadimFarutin <vfarut@gmail.com>
2020-04-10 14:28:39 +0300

    'num_layers': 5

--- runGMExperiments.py
+++ runGMExperiments.py
@@ -76,16 +76,16 @@ def test_GM18526():
                         },
                         output_marks=[output_mark],
                         train_params={
-                            'nb_epoch': 2,
-                            'batch_size': 100,
+                            'nb_epoch': 4,
+                            'batch_size': 128,
                             'validation_split': 0.2,
                             'wandb_log': wandb_log
                         },
                         predict_binary_output=predict_binary_output,
                         zero_out_non_bins=True,
                         generate_bigWig=False,
-                        #pretrained_model_path=None)
-                        pretrained_model_path='./models/weights/lstm-20200409-213314988570-weights.pt')
+                        pretrained_model_path=None)
+                        #pretrained_model_path='./models/weights/lstm-20200409-213314988570-weights.pt')
 
                     if wandb_log:
                         group = "peaks" if predict_binary_output else "signal"
