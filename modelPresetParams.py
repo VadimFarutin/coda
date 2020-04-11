4ae00f4f7ad1bfffe4d26e561772b2a862ecaeec
VadimFarutin <vfarut@gmail.com>
2020-04-12 00:59:41 +0300

    'hidden_size': 16

--- modelPresetParams.py
+++ modelPresetParams.py
@@ -62,7 +62,7 @@ MODEL_PRESET_PARAMS = {
         'model_class': 'SeqToSeq',
         'model_type': 'encoder-decoder',
         'model_specific_params': {
-            'hidden_size': 32,
+            'hidden_size': 16,
             'num_layers': 1,
             'bidirectional': True,
             'teacher_forcing': 0.0,
