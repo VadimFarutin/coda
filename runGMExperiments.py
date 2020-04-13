cb3dae0f3d8fea86aa63ce0a4064be1623328300
VadimFarutin <vfarut@gmail.com>
2020-04-13 19:11:50 +0300

    Resume training

--- runGMExperiments.py
+++ runGMExperiments.py
@@ -88,8 +88,8 @@ def test_GM18526():
                         predict_binary_output=predict_binary_output,
                         zero_out_non_bins=True,
                         generate_bigWig=False,
-                        pretrained_model_path=None)
-                        #pretrained_model_path='./models/weights/cnn-encoder-decoder-20200413-131717465962-weights.pt')
+                        #pretrained_model_path=None)
+                        pretrained_model_path='./models/weights/cnn-encoder-decoder-20200413-143520714403-weights.pt')
 
                     if wandb_log:
                         group = "peaks" if predict_binary_output else "signal"
