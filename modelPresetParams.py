2a9b083d1eab6e867b6d2e78035f9d61b527a8af
VadimFarutin <vfarut@gmail.com>
2020-04-15 14:27:20 +0300

    'hidden_size': 64

--- modelPresetParams.py
+++ modelPresetParams.py
@@ -83,7 +83,7 @@ MODEL_PRESET_PARAMS = {
         'model_class': 'SeqToSeq',
         'model_type': 'cnn-encoder-decoder',
         'model_specific_params': {
-            'hidden_size': 80,
+            'hidden_size': 64,
             'kernel_size': 51,
             'stride': 1,
             'dilation': 2,
