e6d0dd7e94569ecc2e272017c1149527b3d8fa6c
VadimFarutin <vfarut@gmail.com>
2020-04-16 23:22:00 +0300

    'kernel_size': 51

--- modelPresetParams.py
+++ modelPresetParams.py
@@ -84,7 +84,7 @@ MODEL_PRESET_PARAMS = {
         'model_type': 'cnn-encoder-decoder',
         'model_specific_params': {
             'hidden_size': 8,
-            'kernel_size': 11,
+            'kernel_size': 51,
             'stride': 1,
             'dilation': 2,
             'num_layers': 2,
