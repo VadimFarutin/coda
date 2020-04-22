d22c643fff45dd785ea3b0bfc4c3f35842328682
VadimFarutin <vfarut@gmail.com>
2020-04-22 19:00:51 +0300

    predict binary

--- runGMExperiments.py
+++ runGMExperiments.py
@@ -43,7 +43,7 @@ def test_GM18526():
 
     for test_cell_line in ['GM18526']:
         for subsample_target_string in ['0.5e6']:
-            for predict_binary_output in [False]:
+            for predict_binary_output in [True]:
                 for output_mark in GM_MARKS:                            
                     model_type = 'cnn-encoder-decoder'
                     wandb_log = True
@@ -70,7 +70,7 @@ def test_GM18526():
                             'test_dataset_name': '%s_5+1marks-K4me3_all' % test_cell_line,
                             'num_train_examples': 100000,
                             'seq_length': 1001,
-                            'peak_fraction': 0.7,
+                            'peak_fraction': 0.5,
                             'train_X_subsample_target_string': subsample_target_string,
                             'num_bins_to_test': None,
                             'train_chroms': HG19_ALL_CHROMS,
@@ -79,7 +79,7 @@ def test_GM18526():
                         },
                         output_marks=[output_mark],
                         train_params={
-                            'nb_epoch': 1,
+                            'nb_epoch': 5,
                             'batch_size': 100,
                             'validation_split': 0.2,
                             'wandb_log': wandb_log,
@@ -88,8 +88,8 @@ def test_GM18526():
                         predict_binary_output=predict_binary_output,
                         zero_out_non_bins=True,
                         generate_bigWig=False,
-                        #pretrained_model_path=None)
-                        pretrained_model_path='./models/weights/cnn-encoder-decoder-20200422-101735917753-weights.pt')
+                        pretrained_model_path=None)
+                        #pretrained_model_path='./models/weights/cnn-encoder-decoder-20200422-101735917753-weights.pt')
 
                     if wandb_log:
                         group = "peaks" if predict_binary_output else "signal"
