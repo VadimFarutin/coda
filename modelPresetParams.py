9d15abbbb11b2a6fb0380edc9bf6ea4631725070
VadimFarutin <vfarut@gmail.com>
2020-04-24 11:25:30 +0300

    'num_layers': 3, 'residual': True

--- modelPresetParams.py
+++ modelPresetParams.py
@@ -107,8 +107,8 @@ MODEL_PRESET_PARAMS = {
             'kernel_size': 51,
             'stride': 1,
             'dilation': 2,
-            'num_layers': 5,
-            'residual': False,
+            'num_layers': 3,
+            'residual': True,
             'dropout': 0.4,
 
             'disc_hidden_size': 16,
--- runGMExperiments.py
+++ runGMExperiments.py
@@ -80,7 +80,7 @@ def test_GM18526():
                         output_marks=[output_mark],
                         #input_marks=[output_mark, 'INPUT'],
                         train_params={
-                            'nb_epoch': 25,
+                            'nb_epoch': 5,
                             'batch_size': 100,
                             'validation_split': 0.2,
                             'wandb_log': wandb_log,
