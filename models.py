aafdefd8e6d7442838489c8f86a831b76b15220c
VadimFarutin <vfarut@gmail.com>
2020-04-26 01:08:22 +0300

    Update models.py

--- models.py
+++ models.py
@@ -438,7 +438,6 @@ class SeqModel(object):
         if self.model_params['model_type'] == 'adv-cnn-encoder-decoder':
             lr = self.model_params['compile_params']['lr']
             gen_optimizer = optim.Adam(self.model.parameters(), lr=lr)
-            disc_optimizer = optim.Adam(self.model.discriminator.parameters(), lr=lr / 2)
             disc_optimizer = optim.Adam(self.model.discriminator.parameters(), lr=lr * 5)
             disc_loss_function = torch.nn.modules.loss.BCELoss()
             output_marks_idx = [self.input_marks.index(output_mark) for output_mark in self.output_marks]
