124b6561f1a82a9531970c4bc5a72cfcab0fa6f0
VadimFarutin <vfarut@gmail.com>
2020-05-03 22:18:39 +0300

    no xcor

--- scripts/getSignalTrack.sh
+++ scripts/getSignalTrack.sh
@@ -6,12 +6,14 @@ tagAlignPath=$2
 outputDir=$3
 species=$4
 
+#    -final_stage xcor \
+#    -subsample_xcor "10M" \
+
 bds ${pipelineDir}/chipseq.bds \
     -out_dir ${outputDir} \
     -histone \
     -input tag \
-    -final_stage xcor \
-    -subsample_xcor "10M" \
+    -final_stage tag \
     -tag1 ${tagAlignPath} \
     -tag2bw \
     -species ${species} \
